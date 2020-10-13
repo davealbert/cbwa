34ce45ae7f2f5021f5271f20ff7ec334a6067d77
Dave Albert <dave.albert@nearform.com>
2020-10-13 07:45:18 +0100

    User Access settings with x-api-key in database.

--- index.js
+++ index.js
@@ -7,12 +7,49 @@ const port = process.env.PORT || 3000;
 const authorController = require("./controllers/authors")();
 const booksController = require("./controllers/books")();
 
+const users = require("./models/users")();
+
 const app = (module.exports = express());
 
 // logging
 app.use((req, res, next) => {
   // Display log for requests
   console.log("[%s] %s -- %s", new Date(), req.method, req.url);
+  next();
+});
+
+app.use(async (req, res, next) => {
+  const FailedAuthMessage = {
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/401
+    error: "Failed Authentication",
+    message: "Go away!",
+    code: "xxx", // Some useful error code
+  };
+
+  const suppliedKey = req.headers["x-api-key"];
+  req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+
+  // Check Pre-shared key
+  if (!suppliedKey) {
+    console.log(
+      "  [%s] FAILED AUTHENTICATION -- %s, No Key Supplied",
+      new Date(),
+      clientIp
+    );
+    FailedAuthMessage.code = "01";
+    return res.status(401).json(FailedAuthMessage);
+  }
+
+  const user = await users.getByKey(suppliedKey);
+  if (!user) {
+    console.log(
+      "  [%s] FAILED AUTHENTICATION -- %s, BAD Key Supplied",
+      new Date(),
+      clientIp
+    );
+    FailedAuthMessage.code = "02";
+    return res.status(401).json(FailedAuthMessage);
+  }
   next();
 });
 
